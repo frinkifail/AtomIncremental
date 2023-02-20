88c408f4fc4b4db6964768db28b709a523f193cc
frinkifail <frinkifail@cat.cat>
2023-02-20 21:39:18 +0700

    displays + font awesome

--- js/data.js
+++ js/data.js
@@ -5,11 +5,11 @@ const data = {
         helium: {unlocked: false, amount: 0},
         phosphor: {unlocked: false, amount: 0}
     }, // basically materials
-    electrons: 0, // basically money
+    electrons: 10, // basically money
     generators: {
-        oxygen: {unlocked: true, amount: 0, cost: 10},
-        hydrogen: {unlocked: false, amount: 0, cost: 125},
-        helium: {unlocked: false, amount: 0, cost: 350},
-        phosphor: {unlocked: false, amount: 0, cost: 720}
+        oxygen: {unlocked: true, amount: 1, cost: 10, growth: 1.08},
+        hydrogen: {unlocked: false, amount: 0, cost: 125, growth: 1.1},
+        helium: {unlocked: false, amount: 0, cost: 350, growth: 1.17},
+        phosphor: {unlocked: false, amount: 0, cost: 720, growth: 1.2}
     }
 }
--- js/game.js
+++ js/game.js
@@ -1,7 +1,20 @@
-const atom_counter_display = document.getElementById('atom-counter')
+const atom_counter_oxygen = document.getElementById('atom-counter-oxygen')
+const atom_counter_hydrogen = document.getElementById('atom-counter-hydrogen')
+const atom_counter_helium = document.getElementById('atom-counter-helium')
+const atom_counter_phosphor = document.getElementById('atom-counter-phosphor')
+const atom_counter_electron = document.getElementById('atom-counter-electron')
 
 const update_interval = setInterval(function(){
     // atom_counter_display.innerText =
+    data.atoms.oxygen.amount += data.generators.oxygen.amount*1.8
+    atom_counter_oxygen.innerText = data.atoms.oxygen.amount.toLocaleString()
+    data.atoms.hydrogen.amount += data.generators.hydrogen.amount*1.62
+    atom_counter_hydrogen.innerText = data.atoms.hydrogen.amount.toLocaleString()
+    data.atoms.helium.amount += data.generators.helium.amount*1.338
+    atom_counter_helium.innerText = data.atoms.helium.amount.toLocaleString()
+    data.atoms.phosphor.amount += data.generators.phosphor.amount*1.447
+    atom_counter_phosphor.innerText = data.atoms.phosphor.amount.toLocaleString()
+    atom_counter_electron.innerText = data.electrons.toLocaleString()
 }, 500) // update function here // basically main interval
 
 
