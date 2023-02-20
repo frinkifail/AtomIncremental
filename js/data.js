60477f4c1317b85f491ffd46fadcecadce199223
frinkifail <frinkifail@cat.cat>
2023-02-20 18:58:27 +0700

    good

---
+++ js/data.js
@@ -0,0 +1,15 @@
+const data = {
+    atoms: {
+        oxygen: {unlocked: true, amount: 0},
+        hydrogen: {unlocked: false, amount: 0},
+        helium: {unlocked: false, amount: 0},
+        phosphor: {unlocked: false, amount: 0}
+    }, // basically materials
+    electrons: 0, // basically money
+    generators: {
+        oxygen: {unlocked: true, amount: 0, cost: 10},
+        hydrogen: {unlocked: false, amount: 0, cost: 125},
+        helium: {unlocked: false, amount: 0, cost: 350},
+        phosphor: {unlocked: false, amount: 0, cost: 720}
+    }
+}
---
+++ js/game.js
@@ -0,0 +1,7 @@
+const atom_counter_display = document.getElementById('atom-counter')
+
+const update_interval = setInterval(function(){
+    // atom_counter_display.innerText =
+}, 500) // update function here // basically main interval
+
+
