bb509078591aa9adaba866ecd4e72164dc4e57e9
Frinkifail <76682714+frinkifail@users.noreply.github.com>
2023-02-21 15:14:54 +0700

    fluent ui implementationn score: -69/420

--- js/design.js
+++ js/design.js
@@ -4,15 +4,18 @@ let menu_open = true
 const reveal_menu = document.getElementById("reveal-menu")
 const menu = document.getElementById("menu")
 const appbar_main = document.getElementById("appbar-main")
+const fix_appbar = document.getElementById("fixappbar")
 
 reveal_menu.onclick = () => {
     if (menu_open) {
         menu_open = false;
         appbar_main.style.marginLeft = "0px";
+        fix_appbar.style.marginLeft = "0px";
         setTimeout(function(){menu.style.transform = "translateX(-200px)";}, 0)
     } else {
         menu_open = true;
         appbar_main.style.marginLeft = "220px";
+        fix_appbar.style.marginLeft = "220px";
         setTimeout(function(){menu.style.transform = "translateX(0px)";}, 0)
     }
 }
