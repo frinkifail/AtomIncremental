96a0403946c308a0ab01b356e95a71ac7fa4f80e
Frinkifail <76682714+frinkifail@users.noreply.github.com>
2023-02-23 15:35:16 +0700

    you have no idea how long it took me to intergrate firebase into this shit

---
+++ js/firebase_init_app.js
@@ -0,0 +1,65 @@
+// // Initialize the FirebaseUI Widget using Firebase.
+// var ui = new firebaseui.auth.AuthUI(getAuth());
+
+// var uiConfig = {
+//     callbacks: {
+//         signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+//             // User successfully signed in.
+//             // Return type determines whether we continue the redirect automatically
+//             // or whether we leave that to developer to handle.
+//             return true;
+//         },
+//         uiShown: function () {
+//             // The widget is rendered.
+//             // Hide the loader.
+//             document.getElementById('loader').style.display = 'none';
+//         }
+//     },
+//     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+//     signInFlow: 'popup',
+//     signInSuccessUrl: './game',
+//     signInOptions: [
+//         // Leave the lines as is for the providers you want to offer your users.
+//         firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//     ],
+//     // Terms of service url.
+//     tosUrl: '#',
+//     // Privacy policy url.
+//     privacyPolicyUrl: '#'
+// };
+
+// ui.start('#firebaseui-auth-container', uiConfig);
+// 1) Create a new firebaseui.auth instance stored to our local variable ui
+const ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+// 2) These are our configurations.
+const uiConfig = {
+    callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+        },
+        uiShown: function () {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+        }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: './game',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    tosUrl: '#',
+    // Privacy policy url.
+    privacyPolicyUrl: '#'
+}
+
+// 3) Call the 'start' method on our ui class
+// including our configuration options.
+ui.start("#firebaseui-auth-container", uiConfig)
