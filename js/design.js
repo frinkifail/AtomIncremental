48f97b6eb5e459af4f05e97e55cb5f14f92aea36
Frinkifail <76682714+frinkifail@users.noreply.github.com>
2023-03-04 13:57:30 +0700

    bro localhost is dumbbbb for some reason localhost and 127.0.0.1 are showing different things??? and neither of them show the correct thing bruh

--- js/design.js
+++ js/design.js
@@ -6,6 +6,8 @@ const menu = document.getElementById("menu")
 const appbar_main = document.getElementById("appbar-main")
 const fix_appbar = document.getElementById("fixappbar")
 
+let dialog_open = false;
+
 reveal_menu.onclick = () => {
     if (menu_open) {
         menu_open = false;
@@ -21,4 +23,19 @@ reveal_menu.onclick = () => {
 }
 document.getElementById("clickable-nothingreally").onclick = function() {
     document.getElementById("clickable-nothingreally").style.transform  = "translateX(-600px)"
+}
+document.getElementById('open-dialog').onclick = function() {
+    if (dialog_open) {
+        dialog_open = false;
+        document.getElementById('blur-bg').style.display = "none";
+        document.getElementById('card-dialog').style.display = "none";
+    }
+    else {
+        dialog_open = true;
+        document.getElementById('blur-bg').style.display = "initial";
+        document.getElementById('card-dialog').style.display = "initial";
+    }
+}
+document.getElementById('blur-bg').onclick = function() {
+    document.getElementById('open-dialog').onclick()
 }
